bbe22e3c3d50f09c3c7fc614bbe3ed1c6bcebccc
mariusz <mariusz@aetas.pl>
2015-06-16 22:25:14 +0200

    Retriving environment variables corrected for integration and acceptance tests

--- src/test/groovy/com/taskroo/acceptance/AcceptanceTestBase.groovy
+++ src/test/groovy/com/taskroo/acceptance/AcceptanceTestBase.groovy
@@ -11,9 +11,9 @@ import spock.lang.Specification
 abstract class AcceptanceTestBase extends Specification {
 
     static RESTClient client = new RESTClient('http://localhost:8080/')
-    private static final DB db = new MongoConnector(System.properties.getProperty('MONGO_PORT_27017_TCP_ADDR', 'localhost'),
-            System.properties.getProperty('MONGO_PORT_27017_TCP_PORT', '27017'))
-            .getDatabase('taskroo')
+    private static final String MONGO_ADDR = System.getenv('MONGO_PORT_27017_TCP_ADDR') ?: 'localhost'
+    private static final String MONGO_PORT = System.getenv('MONGO_PORT_27017_TCP_PORT') ?: '27017'
+    private static final DB db = new MongoConnector(MONGO_ADDR, MONGO_PORT).getDatabase('taskroo')
     public static final DBCollection tagsCollection = db.getCollection('tags')
     public static final DBCollection tasksCollection = db.getCollection('tasks')
     public static final DBCollection securityTokensCollection = db.getCollection("securityTokens")
